38f0eea5295c993aaa7e7e492ec7b970e15562ea
Harsh Patel <harshpatel231291>
2016-11-23 12:53:14 -0800

    Initial Commit

---
+++ TapWar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapWar
+//
+//  Created by harsh patel on 23/11/16.
+//  Copyright © 2016 harsh patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
